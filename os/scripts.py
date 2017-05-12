b68a954c8c6be61f1e6fc735e105cbee507a76e1
AlanJager <873863981@qq.com>
2017-05-12 09:47:03 +0800

    Update

--- os/scripts.py
+++ os/scripts.py
@@ -9,13 +9,6 @@ def shell(cmd):
         raise e
 
 
-init_cmd = "[ -f ~/.bash_profile ] && source ~/.bash_profile " \
-           "&& [ -f /etc/profile ] && source /etc/profile " \
-           "&& export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:$PATH"
-
-shell(init_cmd)
-
-
 def get_zone_uuid():
     cmd = "$(zstack-cli QueryZone fields=uuid | awk -F ',|:|\"' '/uuid {print $5}' | head -1)"
     return shell(cmd)
@@ -34,9 +27,16 @@ def delete_vxlan_pool():
     vxlan_pool_uuids = "zstack-cli QueryL2VxlanNetworkPool name~=test fields=uuid " \
                   "| jq '.[\"inventories\"][].\"uuid\"' | sed 's/\"//g'"
 
-    for uuid in vxlan_pool_uuids:
+    uuids = vxlan_pool_uuids.split(' ')
+    for uuid in uuids:
         delete_cmd = "zstack-cli DeleteL2Network uuid=%s" % uuid
         shell(delete_cmd)
+
+
+init_cmd = "[ -f ~/.bash_profile ] && source ~/.bash_profile " \
+           "&& [ -f /etc/profile ] && source /etc/profile " \
+           "&& export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:$PATH"
+shell(init_cmd)
 
 create_vxlan_pool(100)
 delete_vxlan_pool()
