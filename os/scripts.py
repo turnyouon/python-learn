71d2daed88d9821882b883a5d5346de075bd49df
AlanJager <873863981@qq.com>
2017-05-16 23:08:27 +0800

    Fix get uuid error

--- os/scripts.py
+++ os/scripts.py
@@ -1,34 +1,39 @@
-import os
-import multiprocessing
+import subprocess
 
 
 def shell(cmd):
-    try:
-	print cmd
-    except Exception as e:
-        raise e
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    std_out, std_err = process.stdout, process.stderr
+
+    errors = std_err.read()
+    if errors:
+        pass
+    result_str = std_out.read().strip()
+    if std_out:
+        std_out.close()
+    if std_err:
+        std_err.close()
+
+    return result_str
 
 
 def get_zone_uuid():
-    cmd = "$(zstack-cli QueryZone fields=uuid " \
-	  "| jq '.[\"inventories\"][].\"uuid\"' | sed 's/\"//g'"
+    cmd = "zstack-cli QueryZone fields=uuid | grep 'uuid' | awk -F ',|:|\"' '{ print $5 }'"
     return shell(cmd)
 
 
 def create_vxlan_pool(num=100):
     zone_uuid = get_zone_uuid()
-    vxlan_pool_name = "test-vxlan-%s"
     for i in range(num):
-        create_vxlan_pool_cmd = "zstack-cli CreateL2VxlanNetworkPool zoneUuid=%s name=test-vxlan-%s" % (zone_uuid, i) 
-
+        create_vxlan_pool_cmd = "zstack-cli CreateL2VxlanNetworkPool zoneUuid=%s name=test-vxlan-%s" % (zone_uuid, i)
         shell(create_vxlan_pool_cmd)
 
 
 def delete_vxlan_pool():
-    vxlan_pool_uuids = "zstack-cli QueryL2VxlanNetworkPool name~=test fields=uuid " \
-                  "| jq '.[\"inventories\"][].\"uuid\"' | sed 's/\"//g'"
+    vxlan_pool_uuids = "zstack-cli QueryL2VxlanNetworkPool name~=test " \
+                       "fields=uuid | grep uuid | awk -F ',|\"|:' '{print $5}'"
 
-    uuids = vxlan_pool_uuids.split(' ')
+    uuids = shell(vxlan_pool_uuids).split(' ')
     for uuid in uuids:
         delete_cmd = "zstack-cli DeleteL2Network uuid=%s" % uuid
         shell(delete_cmd)
@@ -38,7 +43,6 @@ init_cmd = "[ -f ~/.bash_profile ] && source ~/.bash_profile " \
            "&& [ -f /etc/profile ] && source /etc/profile " \
            "&& export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:$PATH"
 shell(init_cmd)
-
 create_vxlan_pool(100)
 delete_vxlan_pool()
 
